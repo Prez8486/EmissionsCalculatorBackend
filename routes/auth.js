7689a1c567cab3dde67c8284a1f1cf891b29b616
Nick <87658345+Prez8486@users.noreply.github.com>
2025-10-08 20:48:28 +1100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -31,4 +31,21 @@ router.post("/change-password", auth, async (req, res) => {
         res.status(500).json({ error: "Failed to change password" });
     }
 });
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    car: {
+        make: { type: String, default: '' },
+        model: { type: String, default: '' },
+        extraLoad: { type: String, default: 'none' },
+
+    }
+
+});
+
+module.exports = mongoose.model("User", userSchema);
+
 module.exports = router;
