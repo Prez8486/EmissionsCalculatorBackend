a239e2f5efa3c677c117751b85936b784712637f
nprez <nprez@DESKTOP-UB1KI4V>
2025-10-28 01:31:24 +1100

    Fix

--- routes/friends.js
+++ routes/friends.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const auth = require("../middleware/auth");
+const auth = require("../middleware/authMiddleware");  
 
 // Send a friend request
 router.post("/request/:friendId", auth, async (req, res) => {
